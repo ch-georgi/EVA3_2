c7664c229c01ef79b9bdcb875f0ac21746905d02
AlvaroWALOT <134461174+AlvaroWALOT@users.noreply.github.com>
2024-07-10 23:07:11 -0400

    fix raiz muestra index (#1)
    merge de branch ??

--- PeludosFacheros/tienda/urls.py
+++ PeludosFacheros/tienda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path("", views.index, name='index'),
     path('contacto', views.contacto, name='contacto'),
     path('donaciones', views.donaciones, name='donaciones'),
     path('nosotros', views.nosotros, name='nosotros'),
