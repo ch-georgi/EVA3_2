2e571ce863b6c5799577faff5e212c34d4e8849e
AlvaroWALOT <alv.castroc@duocuc.cl>
2024-07-10 22:53:07 -0400

    fix raiz muestra index

--- PeludosFacheros/PeludosFacheros/urls.py
+++ PeludosFacheros/PeludosFacheros/urls.py
@@ -21,5 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tienda/', include('tienda.urls'))
+    path("", include('tienda.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
